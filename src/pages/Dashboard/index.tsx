467990f694f3b66ced9f70d728c1aca2d140ee68
fernangb <gfsouza@poli.ufrj.br>
2020-08-11 20:33:25 -0300

    carrinho de compras

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -35,14 +35,17 @@ const Dashboard: React.FC = () => {
 
   useEffect(() => {
     async function loadProducts(): Promise<void> {
-      // TODO
+      const response = await api.get('/products');
+        setProducts(response.data);
+        console.log("Response: ", response)
     }
 
     loadProducts();
+    console.log("Produtos: ", products);
   }, []);
 
   function handleAddToCart(item: Product): void {
-    // TODO
+    addToCart(item);
   }
 
   return (
